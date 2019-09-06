e97613f5a182d59ce3dfb26d2373df4ff3fe95f6
Sébastien BEAU <sebastien.beau@akretion.com>
2019-09-06 13:06:57 +0200

    [REF] refactor bank_transfer payment
    Rename payment bank_transfer into payment_manual
    Add acquired for check
    Move demo data
    Create a transaction in pending state for all manual payment

---
+++ shopinvader_payment_manual/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Payment Manual",
+    "description": """
+        REST Services for manual payment (like bank transfer, check...)""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/shopinvader/odoo-shopinvader-payment",
+    "depends": [
+        "invader_payment_manual",
+        "shopinvader_payment",
+        "payment_transfer",
+    ],
+    "autoinstall": True,
+    "installable": True,
+}
