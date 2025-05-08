355ca314ae19cb2f3e8c03321e08f3f3c100e04f
Simone Orsi <simahawk@gmail.com>
2025-05-08 13:07:23 +0200

    shopinvader_payment: raise error if method not configured

--- shopinvader_payment/models/sale_order.py
+++ shopinvader_payment/models/sale_order.py
@@ -22,5 +22,14 @@ class SaleOrder(models.Model):
         allowed_shop_methods = self.shopinvader_backend_id.payment_method_ids
         # TODO: this should probably come from the frontend
         selected = allowed_shop_methods.filtered(lambda m: m.provider_id == provider)
+        if not selected.method_id:
+            raise UserError(
+                _(
+                    "No payment method found for provider "
+                    "%(provider)s on backend %(backend)s",
+                    provider=provider.name,
+                    backend=self.shopinvader_backend_id.name,
+                )
+            )
         data["payment_method_id"] = selected.method_id.id
         return data
