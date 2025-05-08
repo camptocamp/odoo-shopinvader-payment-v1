cd6cfcafb8ce26073a0cca95735efb9a5337ede9
Simone Orsi <simahawk@gmail.com>
2025-05-08 11:45:56 +0200

    invader_payment_manual: migrate to v18

--- invader_payment_manual/__manifest__.py
+++ invader_payment_manual/__manifest__.py
@@ -6,12 +6,11 @@
     "summary": """
         REST Services for manual payment like bank transfer,
         check ... (base module)""",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Camptocamp",
     "website": "https://github.com/camptocamp/odoo-shopinvader-payment-v1",
-    "depends": ["invader_payment", "base_rest", "payment_transfer"],
+    "depends": ["invader_payment", "base_rest", "payment_custom"],
     "external_dependencies": {"python": ["cerberus"]},
-    "data": ["data/payment_acquirer_data.xml"],
     "installable": True,
 }
--- invader_payment_manual/services/__init__.py
+++ invader_payment_manual/services/__init__.py
@@ -1,1 +1,2 @@
 from . import payment_manual
+from . import invader_payment_service
